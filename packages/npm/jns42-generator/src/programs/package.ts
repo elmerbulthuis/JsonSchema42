d52cd9297620b81aec98979f4fca8104889e031f
Elmer Bulthuis <elmer@luvdasun.software>
2026-08-03 03:49:12 +0200

    use entryLocatoin

--- packages/npm/jns42-generator/src/programs/package.ts
+++ packages/npm/jns42-generator/src/programs/package.ts
@@ -80,7 +80,7 @@ async function main(configuration: MainConfiguration) {
 
   const context = contextBuilder.build();
 
-  await context.loadFromLocation(
+  const entryLocation = await context.loadFromLocation(
     instanceSchemaLocation,
     instanceSchemaLocation,
     undefined,
@@ -96,6 +96,6 @@ async function main(configuration: MainConfiguration) {
     packageDirectoryPath,
     packageName,
     packageVersion,
-    entryLocation: instanceSchemaLocation,
+    entryLocation,
   });
 }
